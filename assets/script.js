8d14e83563076b387c62118692157fd27c68ea99
unknown <thomasjjohnson9311@gmail.com>
2023-04-19 12:50:12 -0500

    trying to display properly

--- assets/script.js
+++ assets/script.js
@@ -1,25 +1,36 @@
-// Get the modal
-var modal = document.getElementById("myModal");
+// // Get the modal
+// var modal = document.getElementById("myModal");
 
-// Get the button that opens the modal
-var btn = document.getElementById("myBtn");
+// // Get the button that opens the modal
+// var btn = document.getElementById("myBtn");
 
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
+// // Get the <span> element that closes the modal
+// var span = document.getElementsByClassName("close")[0];
 
-// When the user clicks on the button, open the modal
-btn.onclick = function () {
-    modal.style.display = "block";
-}
+// // When the user clicks on the button, open the modal
+// btn.onclick = function () {
+//   modal.style.display = "block";
+// };
 
-// When the user clicks on <span> (x), close the modal
-span.onclick = function () {
-    modal.style.display = "none";
-}
+// // When the user clicks on <span> (x), close the modal
+// span.onclick = function () {
+//   modal.style.display = "none";
+// };
 
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function (event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
-    }
-}
+// // When the user clicks anywhere outside of the modal, close it
+// window.onclick = function (event) {
+//   if (event.target == modal) {
+//     modal.style.display = "none";
+//   }
+// };
+// let newDiv = document.querySelector("div");
+fetch("https://www.boredapi.com/api/activity")
+  .then((response) => response.json())
+  .then((data) => {
+    console.log(data.activity);
+    localStorage.setItem("Activity 1", data.activity);
+  });
+
+console.log(localStorage.getItem("Activity 1"));
+
+console.log(localStorage);
